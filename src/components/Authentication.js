079ca43f9348a95aedb8997695713ecafc21ea8a
Toufiquer <toufiquerabc@gmail.com>
2023-01-30 05:30:26 +0600

    authentication complete with condition

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,58 +1,105 @@
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import googleIcon from "./icon/google.png";
 import facebookIcon from "./icon/facebook.png";
 import githubIcon from "./icon/github.png";
 import auth from "../firebase";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useAuthState, useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import Loading from "./Loading";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 const customId = "custom toast id";
 const Authentication = () => {
   const [toggle, setToggle] = useState(false);
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-  if (loading) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
+  const [createUserWithEmailAndPassword, user2, loading2, error2] = useCreateUserWithEmailAndPassword(auth);
+  const [signInWithEmailAndPassword, user3, loading3, error3] = useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user?.uid) {
+      navigate("/");
+    }
+  });
+  if (loading || loading1 || loading2 || loading3) {
     return <Loading></Loading>;
   }
-  if (error) {
-    toast.error(error.message, {
+  if (error || error1 || error2 || error3) {
+    toast.error(error?.message || error1?.message || error2?.message || error3.message, {
       toastId: customId,
     });
   }
-  if (user) {
+  if (user1 || user2 || user3) {
     toast.success("Successfully Login", {
       toastId: customId,
     });
-    console.log(user);
   }
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (toggle) {
+      if (password !== password2) {
+        toast.warning("Password Doesn't Match", {
+          toastId: customId,
+        });
+      } else {
+        toggle && createUserWithEmailAndPassword(email, password);
+      }
+    }
+    !toggle && signInWithEmailAndPassword(email, password);
   };
   return (
     <div className={`bg-blue-300 min-h-screen w-full p-4`}>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
         <div className="container  mx-auto">
-          <form onClick={handleSubmit} className="flex flex-col gap-2 w-[480px] mx-auto mt-4 border-2 border-solid border-blue-600 rounded p-4">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-[480px] mx-auto mt-4 border-2 border-solid border-blue-600 rounded p-4">
             <h2 className={`font-thin text-6xl my-4 text-center`}>{toggle ? <span>Please Register</span> : <span>Please Log In</span>}</h2>
             <div className="w-full h-[1px] bg-blue-600 mb-2 mt-[-5px]"></div>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="email2" value="Your email" />
               </div>
-              <TextInput id="email2" type="email" placeholder="name@flowbite.com" required={true} shadow={true} />
+              <TextInput
+                onBlur={(e) => {
+                  setEmail(e.target.value);
+                }}
+                id="email2"
+                type="email"
+                placeholder="name@flowbite.com"
+                required={true}
+                shadow={true}
+              />
             </div>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="password2" value="Your password" />
               </div>
-              <TextInput id="password2" type="password" required={true} shadow={true} />
+              <TextInput
+                onBlur={(e) => {
+                  setPassword(e.target.value);
+                }}
+                id="password2"
+                type="password"
+                required={true}
+                shadow={true}
+              />
             </div>
             {toggle && (
               <div>
                 <div className="mb-2 block">
                   <Label htmlFor="repeat-password" value="Repeat password" />
                 </div>
-                <TextInput id="repeat-password" type="password" required={true} shadow={true} />
+                <TextInput
+                  onBlur={(e) => {
+                    setPassword2(e.target.value);
+                  }}
+                  id="repeat-password"
+                  type="password"
+                  required={true}
+                  shadow={true}
+                />
               </div>
             )}
             {toggle && (
@@ -81,7 +128,7 @@ const Authentication = () => {
           <h2 className={`font-thin text-4xl text-center`}>Or Continue With</h2>
           <div className="w-[480px] mx-auto h-[1px] bg-blue-600 mb-2"></div>
           <div className="flex items-center justify-center flex-col w-[480px] mx-auto gap-y-4">
-            <Button onClick={() => signInWithGoogle()} type="submit" className={`w-full`}>
+            <Button onClick={() => signInWithGoogle()} className={`w-full`}>
               {" "}
               <span className={`h-6 w-6 mx-2`}>
                 {" "}
@@ -89,7 +136,7 @@ const Authentication = () => {
               </span>{" "}
               Continue With Google
             </Button>
-            <Button type="submit" className={`w-full`}>
+            <Button className={`w-full`}>
               {" "}
               <span className={`h-6 w-6 mx-2`}>
                 {" "}
@@ -97,7 +144,7 @@ const Authentication = () => {
               </span>{" "}
               Continue With Facebook
             </Button>
-            <Button type="submit" className={`w-full`}>
+            <Button className={`w-full`}>
               {" "}
               <span className={`h-6 w-6 mx-2`}>
                 {" "}
